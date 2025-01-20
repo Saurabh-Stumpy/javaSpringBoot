b09ffdf0b686b1e7d9beeec366d51b035dca2078
Saurabh-Stumpy <walanjuwani@gmail.com>
2025-01-20 22:26:44 +0530

    Redis Config

--- demo-redis/src/main/java/com/example/demo_redis/config/RedisConfig.java
+++ demo-redis/src/main/java/com/example/demo_redis/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package com.example.demo_redis.config;
 
+import model.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
@@ -38,5 +42,36 @@ public class RedisConfig {
     }
 
     // Redis Template :For executing any command we need Redis Template.
+    /*
+    *   RedisTemplate<K,V>
+    *   K -> DataType of Key
+    *   V -> DataType of the value
+    *
+    *   In RedisTemplate there are multiple operations :
+    *   valueOps, listOps, setOps, etc.
+    *
+    * */
+
+
+    @Bean
+    public RedisTemplate<String, Person> getTemplate(){
+        RedisTemplate<String,Person> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(getConnection());
+
+        // To convert Java Object to String to store it on server
+        // StringRedisSerializer - converts the Java String to Redis String
+        // converts to bytes and Deserialize converts bytes to Java String
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // JdkSerializationRedisSerializer - Take JAVA Object and converts to Byte
+        // And takes Bytes and converts to JAVA object
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+
+
+        return  redisTemplate;
+    }
+
+
+
 
 }
