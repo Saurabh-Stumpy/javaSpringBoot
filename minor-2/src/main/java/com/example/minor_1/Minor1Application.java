b73393d3fb1506b03214870ddfcf56916f10d40b
Saurabh-Stumpy <walanjuwani@gmail.com>
2025-03-01 11:34:41 +0530

    Adding default admin in minor project

--- minor-2/src/main/java/com/example/minor_1/Minor1Application.java
+++ minor-2/src/main/java/com/example/minor_1/Minor1Application.java
@@ -1,5 +1,9 @@
 package com.example.minor_1;
 
+import com.example.minor_1.models.Admin;
+import com.example.minor_1.models.SecuredUser;
+import com.example.minor_1.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Minor1Application implements CommandLineRunner {
 
+	@Autowired
+	AdminService adminService;
+
 	public static void main(String[] args)  {
 		SpringApplication.run(Minor1Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		Admin admin = Admin.builder()
+				.name("Ram")
+				.email("ram@gmail.com")
+				.securedUser(
+						SecuredUser.builder()
+								.username("ram")
+								.password("ram123")
+								.build()
+				)
+				.build();
+//		adminService.create(admin);
 
 	}
 }
