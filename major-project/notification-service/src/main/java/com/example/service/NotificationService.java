dced1753b1e136554b5688b2b6eefcbe77c1f82e
Saurabh-Stumpy <walanjuwani@gmail.com>
2025-03-13 08:38:03 +0530

    start of mailnotification

---
+++ major-project/notification-service/src/main/java/com/example/service/NotificationService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    @KafkaListener(topics = ,groupId = )
+    private void
+
+}
